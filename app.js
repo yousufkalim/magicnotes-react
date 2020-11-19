34011285c48501925985696886fe321aed8b6707
Yousuf Kalim <yousufkalim@outlook.com>
2020-11-19 16:31:30 +0500

    Initialized for Deloyment

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const notes = require("./controller/notes");
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+
+//Using build for production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("/client/build"));
+}
 
 //Middleware
 app.use(bodyParser.json());
